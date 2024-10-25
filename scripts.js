99c70ae10257498efea14e105835d1e218c5ebf6
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-10-25 15:57:00 +0000

    If Else condition use to roll dice

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
-function rollDice(){
+function rollDice() {
     const min = 1;
     const max = 6;
 
-    const randomNumber = Math.round(Math.random() * (max-min)) + min;
-    console.log(randomNumber);
+    const randomNum = Math.round(Math.random() * (max - min)) + min;
+
+    const diceImg = document.getElementById('img-dice');
+
+    if (randomNum == 1) {
+        diceImg.src = '1.png';
+    }
+    else if (randomNum == 2) {
+        diceImg.src = '2.png';
+    }
+    else if (randomNum == 3) {
+        diceImg.src = '3.png';
+    }
+    else if (randomNum == 4) {
+        diceImg.src = '4.png';
+    }
+    else if (randomNum == 5) {
+        diceImg.src = '5.png';
+    }
+    else if (randomNum == 6) {
+        diceImg.src = '6.png';
+    }
 }
